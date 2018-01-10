c12617e41bbeaed88b79d5e3baa04b19a6ed1e69
Kangalioo <jannik.a.schaper@web.de>
2018-01-10 15:18:14 +0000

    Add and integrate Note class

--- Datasheet.java
+++ Datasheet.java
@@ -5,8 +5,55 @@ import java.io.FileReader;
 import java.io.PrintWriter;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.util.List;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Datasheet {
+	public class Note {
+		public final int x, y;
+		public final int tick;
+		
+		public Note(int x, int y, int tick) {
+			this.x = x;
+			this.y = y;
+			this.tick = tick;
+		}
+		
+		/**
+		 * Converts the tick to the beat, using the ticksPerBeat
+		 * field of the parent Datasheet object.
+		 */
+		public float getBeat() {
+			return tick / (float) ticksPerBeat;
+		}
+		
+		/**
+		 * Converts the tick to the second.
+		 */
+		public float getSecond() {
+			return getBeat() / tempo;
+		}
+		
+		/**
+		 * Returns the index of this note's type in the given
+		 * array of note types (First index if there's the
+		 * same note type twice). -1 if it is not found.
+		 * A note type is the position a note starts on in beats,
+		 * e.g. 1 for notes that are on a beat,
+		 * 0.5 for notes that are between two beats and so on.
+		 */
+		public int getNoteType(List<Float> noteTypes) {
+			for (int i = 0; i < noteTypes.size(); i++) {
+				if ((getBeat() % noteTypes.get(i)) < 1e-5f) {
+					return i;
+				}
+			}
+			return -1;
+		}
+	}
+	
 	private static class Parser {
 		static final String KEY_VALUE_SEPARATOR = ": ";
 		static final String
@@ -37,7 +84,12 @@ public class Datasheet {
 		}
 		
 		private void warn(String msg) {
-			System.err.println("Warning (" + file.getName() + ":" + (currentLine + 1) + "): " + msg);
+			System.err.println("Warning ("
+					+ file.getName()
+					+ ":"
+					+ (currentLine + 1)
+					+ "): "
+					+ msg);
 		}
 		
 		private void processLine(String line) {
@@ -48,7 +100,8 @@ public class Datasheet {
 			}
 			
 			String key = line.substring(0, separatorIndex);
-			String value = line.substring(separatorIndex + KEY_VALUE_SEPARATOR.length());
+			int valueStartIndex = separatorIndex + KEY_VALUE_SEPARATOR.length();
+			String value = line.substring(valueStartIndex);
 			
 			switch (key) {
 				case NAME_KEY: parseName(value); break;
@@ -92,23 +145,17 @@ public class Datasheet {
 		}
 	}
 	
-	private String name;
-	private float tempo;
-	
-	public Datasheet() {
-		this("Unnamed", 120);
-	}
-	
-	public Datasheet(String name, float tempo) {
-		setName(name);
-		setTempo(tempo);
-	}
+	private String name = null;
+	private float tempo = 120;
+	private int ticksPerBeat = 48;
+	private List<Note> notes = new ArrayList<>();
 	
 	public static Datasheet fromFile(File file) throws IOException {
 		return new Parser(file).parse();
 	}
 	
-	public static void writeToFile(Datasheet sheet, File target) throws IOException {
+	public static void writeToFile(Datasheet sheet, File target)
+									throws IOException {
 		new Writer(sheet, target).write();
 	}
 	
@@ -120,4 +167,17 @@ public class Datasheet {
 	public void setName(String name) {this.name = name;}
 	public float getTempo() {return tempo;}
 	public void setTempo(float tempo) {this.tempo = tempo;}
+	public List<Note> getNotes() {return notes;}
+	public void addNote(Note note) {notes.add(note);}
+	
+	/**
+	 * Sorts the notes in the order that they appear.
+	 */
+	public void sortNotes() {
+		Collections.sort(notes, new Comparator<Note>() {
+			public int compare(Note a, Note b) {
+				return a.tick - b.tick;
+			}
+		});
+	}
 }
